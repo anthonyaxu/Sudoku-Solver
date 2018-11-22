0856ac14a872d76faecb8fc2d569a19a740a37c5
Anthony <anthony.a.xu@hotmail.com>
2018-11-22 17:07:23 +1100

    Created main file and input text files

---
+++ solver.c
@@ -0,0 +1,19 @@
+// C program that solves a Sudoku puzzle
+// Created by Anthony Xu, 22nd November 2018
+// https://github.com/anthonyaxu/Sudoku-Solver
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+	// Makes sure that a text file has been inputed to program
+	if (argc != 2) {
+		printf("Usage: %s <input file>\n", argv[0]);
+		return 1;
+	}
+
+	FILE *f = fopen();
+
+	return 0;
+}
